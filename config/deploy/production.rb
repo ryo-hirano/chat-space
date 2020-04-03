97d89f125919eadef10e8a69fec96e3a6a8673e0
ryo-hirano <62536923+ryo-hirano@users.noreply.github.com>
2020-04-03 21:32:44 +0900

    デプロイ文字修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
 server '18.177.85.148', user: 'ec2-user', roles: %w{app db web}
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
